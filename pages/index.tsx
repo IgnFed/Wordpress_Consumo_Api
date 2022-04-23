64269c37a054a0bfc119186ee6edaed422242edc
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-23 10:21:22 -0300

    changed styles, and fixed errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,56 @@
-import { useState } from "react"
-import useApi from "../hooks/useApi"
-import { Input } from '../components/Input'
-import { Form } from "../components/Form"
-import { DataList } from "../components/DataList"
+import React, { useState } from "react"
+import useApi from "hooks/useApi"
+import { Input } from 'components/Input'
+import { Form } from "components/Form"
+import { DataList } from "components/DataList"
 import { Loader } from "components/Loader"
+import { Button } from "components/Button"
 export default function Home() {
 
   const [searcherValue, setSearcherValue] = useState('')
-  const [ isEnable, setIsEnable ] = useState(false)
-  const { data, isLoading, updateFilters, abort } = useApi({})
-  const handleChange = (e: any) =>{
-    if(!e.target.value) setIsEnable(false)
-    else setIsEnable(true)
+  const [isDisable, setIsDisable] = useState(true)
+  const { response, isLoading, updateFilters, abort } = useApi({})
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value) setIsDisable(true)
+    else setIsDisable(false)
     setSearcherValue(e.target.value)
   }
-  const handleSubmit = (e:any)=>{
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     updateFilters({ search: searcherValue })
-    setIsEnable(false)
+    setIsDisable(false)
   }
 
-  const handleAbort = ()=>{
-    setIsEnable(true)
+  const handleAbort = () => {
+    if (!searcherValue) setIsDisable(true)
+    else setIsDisable(false)
     abort()
   }
 
   return (
-    <section className="h-full flex flex-col items-center pt-3 gap-10">
+    <section className="p-2 grid grid-rows-[150px_1fr] gap-10">
       <Form submitEvent={handleSubmit} >
         <Input handleChange={handleChange} value={searcherValue} />
-        <button type="submit" disabled={!isEnable ? true : false} className={`bg-gray-700 w-10/12 p-3 rounded-md text-gray-400 ${isEnable && 'hover:bg-gray-800'} ${isEnable && 'hover:text-white'} transition-colors sm:w-2/5`} >Search</button>
+        <Button disabled={isDisable} type="submit" className="w-10/12 sm:w-2/5">Search</Button>
       </Form>
       {
-        isLoading ? 
-        <>
-        <Loader />
-        <button type="button" onClick={handleAbort} className={` absolute top-1/2 left-1/2 translate-y-20 -translate-x-1/2 bg-gray-700 w-2/5 p-3 rounded-md text-gray-400 ${isEnable && 'hover:bg-gray-800'} ${isEnable && 'hover:text-white'} transition-colors`} >Cancel</button>
-        </>
-        
-        :
-        <DataList data={data} />
+        isLoading ?
+
+          <div className="flex flex-col w-fit justify-self-center items-center gap-2 pt-10">
+            <Loader />
+            <Button onClick={handleAbort} className="w-56 sm:w-96">Cancel</Button>
+          </div>
+
+          :
+
+          !response.size && !isLoading ?
+
+            <span className="text-center text-gray-600 text-2xl">¡No hay artículos relacionados con el término de búsqueda!</span>
+
+            :
+
+            <DataList data={response.data} />
+
       }
     </section>
   )
