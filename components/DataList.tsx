a356e58efcf289aeb3fd9f34bf01bec00dbbeb4d
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-23 10:15:06 -0300

    classNames & interfaces changed

--- components/DataList.tsx
+++ components/DataList.tsx
@@ -1,19 +1,12 @@
 import Image from 'next/image'
-import { DataResponse } from "interfaces/DataResponse"
+import { Data } from "interfaces/DataResponse.interface"
 import Link from 'next/link'
 
-
-
-interface DataListProps {
-  data: Array<DataResponse>
-}
-
-
-const DataItem = <Props extends DataResponse>(
+const DataItem = <Props extends Data>(
   props: Props
 ) => {
   return (
-    <li className="text-white bg-slate-500 p-2 rounded-md text-center">
+    <li className="text-white pt-1 bg-slate-500 rounded-md text-center">
       <Link href='#' passHref>
         <a>
           <Image
@@ -35,16 +28,20 @@ const DataItem = <Props extends DataResponse>(
   )
 }
 
+interface DataListProps {
+  data: Array<Data>
+}
+
 
 export const DataList = <Props extends DataListProps>(
   {
-    data = [{}] as Array<DataResponse>,
+    data = [] as Props["data"],
   }: Props
 ) => {
   return (
-    <ul className="grid w-full grid-cols-auto-fill place-content-center gap-3 m-2 sm:mr-6 sm:ml-6" >
+    <ul className="grid w-full grid-cols-auto-fill place-content-center gap-3" >
       {
-        data.map((item: DataResponse, idx: number) => (
+        data.map((item: Data, idx: number) => (
 
           <DataItem key={item.id + idx} {...item} />
 
