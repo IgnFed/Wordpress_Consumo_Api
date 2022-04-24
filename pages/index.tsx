5a5052359cfbba194161295c9c10241b5975de6e
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-24 20:59:27 -0300

    UI added and filters data managment improved

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,21 @@ import { Button } from "components/Button"
 import { NoData } from "components/NoData"
 import { DataResponse } from "interfaces/DataResponse.interface"
 import { Filters } from "interfaces/hooks/useApi.interface"
+import { Select } from "components/Select"
+import ReactPaginate from "react-paginate"
+
 export default function Home() {
 
-  const [inputValues, setInputValues] = useState({
+  const [filters, setFilters] = useState<Filters>({
     search: '',
-    orderby: ''
+    orderby: 'date',
+    page: 1,
+    order: 'desc',
   })
-  const [isDisable, setIsDisable] = useState(true)
-  const [filters, setFilters] = useState<Filters>({ search: '', page: 1, orderby: 'relevance' })
-  const { updateFilters, statusResponse, response, abort } = useApi<DataResponse>("https://beta.mejorconsalud.com/wp-json/mc/v3/posts", true)
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.value) setIsDisable(true)
-    else setIsDisable(false)
-    setInputValues((prev)=> ({...prev, [e.target.name]: e.target.value}))
+  const { updateFilters, statusResponse, response, abort } = useApi<DataResponse>("https://beta.mejorconsalud.com/wp-json/mc/v3/posts", filters, true)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setFilters((prev) => ({ ...prev, [e.target.name]: e.target.value }))
     setFilters({ ...filters, [e.target.name]: e.target.value })
   }
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -29,35 +31,64 @@ export default function Home() {
   }
 
   const handleAbort = () => {
-    if (!inputValues) setIsDisable(true)
-    else setIsDisable(false)
     abort()
   }
 
+  const handlePageChange = (selectedItem: { selected: number }) => {
+    console.log(selectedItem.selected + 1)
+    setFilters({ ...filters, page: selectedItem.selected + 1 })
+  }
+
   return (
-    <section className="p-2 h-screen grid grid-rows-[250px_1fr] gap-2 overflow-y-auto">
-      <div className="flex flex-cols">
+    <section className="pb-2 pr-2 pl-2 h-screen grid grid-rows-[auto_1fr] overflow-y-auto">
+      <div className="pt-2 pb-2 flex flex-col sticky top-0 z-10 w-full items-center flex-cols gap-2 bg-slate-900">
         <Form submitEvent={handleSubmit} >
-          <div>
           <Input
             handleChange={handleChange}
-            value={inputValues.search}
-            id='searcher'
-            name="searcher"
+            value={filters?.search || ""}
+            id='search'
+            name="search"
             placeholder="Search"
-          >
-            Search
-          </Input>
+          />
+          <div className="flex flex-wrap justify-center gap-2">
+            <Select
+              name="orderby"
+              onChange={handleChange}
+            >
+              <option value="date" defaultChecked >Date</option>
+              <option value="relevance" >Relevance</option>
+            </Select>
+            <Select
+              name="order"
+              onChange={handleChange}
+            >
+              <option value="desc" defaultChecked >Descendence</option>
+              <option value="asc" >Ascendence</option>
+            </Select>
           </div>
-          <Button onClick={handleAbort} disabled={isDisable} type="submit" className="w-10/12 sm:w-2/5">Search</Button>
+          <ReactPaginate
+            pageCount={response?.pages || 1}
+            pageRangeDisplayed={10}
+            initialPage={filters.page! - 1}
+            previousLabel={"←"}
+            nextLabel={"→"}
+            breakLabel={"..."}
+            onPageChange={handlePageChange}
+            containerClassName={"bg-gray-800 p-2 rounded-md flex gap-2 flex-wrap align-middle items-center justify-center"}
+            nextLinkClassName='bg-blue-400 pr-2 pl-2 rounded-md'
+            previousLinkClassName='bg-blue-400 pr-2 pl-2 rounded-md'
+            activeClassName="bg-blue-500 text-center pr-2 pl-2 rounded-md"
+          />
+          <Button onClick={handleAbort} type="submit" className="w-10/12 sm:w-2/5">Search</Button>
         </Form>
+        <p>Total Elements {response?.size || 0}</p>
       </div>
       {
         statusResponse === 'loading' ?
 
           <div className="flex flex-col justify-self-center items-center gap-2 pt-10">
             <Loader />
-            <Button className="w-56 sm:w-96">Cancel</Button>
+            <Button onClick={handleAbort} className="w-56 sm:w-96">Cancel</Button>
           </div>
           :
           statusResponse === 'success' && !response!.size ?
