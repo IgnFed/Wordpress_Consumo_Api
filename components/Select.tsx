f1cc90e44c55544a09d6eeeae487e5ef2e579f24
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-24 20:31:33 -0300

    components created

---
+++ components/Select.tsx
@@ -0,0 +1,22 @@
+import { BasicProps } from "interfaces/components/BasicProps"
+
+interface SelectProps extends BasicProps {
+  name: string
+  required?: boolean,
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+export const Select = <Props extends SelectProps>(
+  {
+    children,
+    name,
+    required,
+    onChange
+  }: Props
+) => {
+  return (
+    <select onChange={onChange} className="text-black p-2 rounded-md" name={name} required={required} >
+      {children}
+    </select>
+  )
+}
