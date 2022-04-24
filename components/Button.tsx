a054a6f8d57442ddeed96a196c2b9cc70b7935cc
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-24 20:58:36 -0300

    improved readability

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,7 +2,7 @@ import { BasicProps } from "interfaces/components/BasicProps";
 
 type ButtonType = 'button' | 'submit' | 'reset';
 
-interface ButtonProps extends BasicProps{
+interface ButtonProps extends BasicProps {
   type?: ButtonType
   className?: string
   disabled?: boolean
@@ -12,11 +12,18 @@ interface ButtonProps extends BasicProps{
 export const Button = <Props extends ButtonProps>(
   {
     children,
-    className='',
-    type='button',
-    disabled=false,
-    onClick=()=>{}
+    className = '',
+    type = 'button',
+    disabled = false,
+    onClick = () => { }
   }: Props
-) =>(
-  <button type={type} onClick={onClick} disabled={disabled} className={`bg-gray-700 p-2 rounded-md text-gray-100 ${disabled ? 'bg-gray-600' : ""} ${disabled ? 'text-slate-400' : ""} transition-colors ${className}`} >{children}</button>
+) => (
+  <button
+    type={type}
+    onClick={onClick}
+    disabled={disabled}
+    className={`bg-gray-700 hover:bg-slate-800 p-2 rounded-md text-gray-100 ${disabled ? 'bg-gray-600' : ""} ${disabled ? 'text-slate-400' : ""} transition-colors ${className}`}
+  >
+    {children}
+  </button>
 )
