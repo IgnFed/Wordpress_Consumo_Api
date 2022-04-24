e72e6fb6696b74ede0fea7eb6d0c4b666f490fc1
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-24 07:19:24 -0300

    No Data message refactored to his own component

---
+++ components/NoData.tsx
@@ -0,0 +1,11 @@
+import { BasicProps } from 'interfaces/components/BasicProps'
+
+export const NoData = <Props extends BasicProps>(
+  {
+    children
+  }: Props
+) => (
+  <div className="w-fit p-2 rounded-md place-self-center bg-neutral-800">
+    <p className="text-center text-gray-600">{children}</p>
+  </div>
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Form } from "components/Form"
 import { DataList } from "components/DataList"
 import { Loader } from "components/Loader"
 import { Button } from "components/Button"
+import { NoData } from "components/NoData"
 export default function Home() {
 
   const [searcherValue, setSearcherValue] = useState('')
@@ -28,7 +29,7 @@ export default function Home() {
   }
 
   return (
-    <section className="p-2 grid grid-rows-[150px_1fr] gap-10">
+    <section className="p-2 h-screen grid grid-rows-[150px_1fr] gap-2 overflow-y-auto">
       <Form submitEvent={handleSubmit} >
         <Input handleChange={handleChange} value={searcherValue} />
         <Button disabled={isDisable} type="submit" className="w-10/12 sm:w-2/5">Search</Button>
@@ -36,7 +37,7 @@ export default function Home() {
       {
         isLoading ?
 
-          <div className="flex flex-col w-fit justify-self-center items-center gap-2 pt-10">
+          <div className="flex flex-col justify-self-center items-center gap-2 pt-10">
             <Loader />
             <Button onClick={handleAbort} className="w-56 sm:w-96">Cancel</Button>
           </div>
@@ -45,8 +46,7 @@ export default function Home() {
 
           !response.size && !isLoading ?
 
-            <span className="text-center text-gray-600 text-2xl">¡No hay artículos relacionados con el término de búsqueda!</span>
-
+            <NoData>¡No hay artículos relacionados con el término de búsqueda!</NoData>
             :
 
             <DataList data={response.data} />
