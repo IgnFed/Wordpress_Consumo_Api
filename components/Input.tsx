167cc72bb15767223de36df3b734ad3c7bc9f284
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-24 16:58:34 -0300

    Generic Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,22 +1,32 @@
+import { BasicProps } from "interfaces/components/BasicProps"
 
-interface InputProps {
+interface InputProps extends BasicProps {
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   value: string
+  id: string
+  name: string
+  placeholder?: string
+  type?: string
 }
 
 export const Input = <Props extends InputProps>(
   {
     handleChange,
-    value
+    value,
+    id,
+    name,
+    children,
+    placeholder,
+    type = 'text'
   }: Props
 ) => {
   return (
-    <label htmlFor="searcher" className=" w-full sm:w-9/12 flex flex-col items-center" >
-      <span>Search</span>
+    <label htmlFor={id} className=" w-full sm:w-9/12 flex flex-col items-center" >
+      <span>{children}</span>
       <input
-        type="text"
-        id="searcher"
-        name="searcher"
+        type={type}
+        id={id}
+        name={name}
         className="placeholder:text-slate-300 text-black w-full max-w-lg font-bold border-none outline-none rounded-md p-3"
         onChange={handleChange}
         value={value} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,15 +10,18 @@ import { DataResponse } from "interfaces/DataResponse.interface"
 import { Filters } from "interfaces/hooks/useApi.interface"
 export default function Home() {
 
-  const [searcherValue, setSearcherValue] = useState('')
+  const [inputValues, setInputValues] = useState({
+    search: '',
+    orderby: ''
+  })
   const [isDisable, setIsDisable] = useState(true)
   const [filters, setFilters] = useState<Filters>({ search: '', page: 1, orderby: 'relevance' })
   const { updateFilters, statusResponse, response, abort } = useApi<DataResponse>("https://beta.mejorconsalud.com/wp-json/mc/v3/posts", true)
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.value) setIsDisable(true)
     else setIsDisable(false)
-    setSearcherValue(e.target.value)
-    setFilters({ ...filters, search: e.target.value })
+    setInputValues((prev)=> ({...prev, [e.target.name]: e.target.value}))
+    setFilters({ ...filters, [e.target.name]: e.target.value })
   }
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -26,16 +29,26 @@ export default function Home() {
   }
 
   const handleAbort = () => {
-    if (!searcherValue) setIsDisable(true)
+    if (!inputValues) setIsDisable(true)
     else setIsDisable(false)
     abort()
   }
 
   return (
-    <section className="p-2 h-screen grid grid-rows-[200px_1fr] gap-2 overflow-y-auto">
+    <section className="p-2 h-screen grid grid-rows-[250px_1fr] gap-2 overflow-y-auto">
       <div className="flex flex-cols">
         <Form submitEvent={handleSubmit} >
-          <Input handleChange={handleChange} value={searcherValue} />
+          <div>
+          <Input
+            handleChange={handleChange}
+            value={inputValues.search}
+            id='searcher'
+            name="searcher"
+            placeholder="Search"
+          >
+            Search
+          </Input>
+          </div>
           <Button onClick={handleAbort} disabled={isDisable} type="submit" className="w-10/12 sm:w-2/5">Search</Button>
         </Form>
       </div>
