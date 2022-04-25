beafadc17d745a5dbe88e10287e269be5e84d0d0
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-24 21:36:05 -0300

    inital filters is now a constant on first render

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,24 @@ import { Form } from "components/Form"
 import { DataList } from "components/DataList"
 import { Loader } from "components/Loader"
 import { Button } from "components/Button"
-import { NoData } from "components/NoData"
+import { DataMessage } from "components/DataMessage"
 import { DataResponse } from "interfaces/DataResponse.interface"
 import { Filters } from "interfaces/hooks/useApi.interface"
 import { Select } from "components/Select"
 import ReactPaginate from "react-paginate"
-
+const INITIAL_FILTERS = {
+  search: '',
+  orderby: 'date',
+  page: 1,
+  order: 'desc',
+} as Filters
 export default function Home() {
 
-  const [filters, setFilters] = useState<Filters>({
-    search: '',
-    orderby: 'date',
-    page: 1,
-    order: 'desc',
-  })
+  const [filters, setFilters] = useState<Filters>(INITIAL_FILTERS)
   const { updateFilters, statusResponse, response, abort } = useApi<DataResponse>("https://beta.mejorconsalud.com/wp-json/mc/v3/posts", filters, true)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     setFilters((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-    setFilters({ ...filters, [e.target.name]: e.target.value })
   }
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -41,7 +40,7 @@ export default function Home() {
 
   return (
     <section className="pb-2 pr-2 pl-2 h-screen grid grid-rows-[auto_1fr] overflow-y-auto">
-      <div className="pt-2 pb-2 flex flex-col sticky top-0 z-10 w-full items-center flex-cols gap-2 bg-slate-900">
+      <section className="pt-2 pb-2 flex flex-col sticky top-0 z-10 w-full items-center flex-cols gap-2 bg-slate-900">
         <Form submitEvent={handleSubmit} >
           <Input
             handleChange={handleChange}
@@ -81,21 +80,27 @@ export default function Home() {
           />
           <Button onClick={handleAbort} type="submit" className="w-10/12 sm:w-2/5">Search</Button>
         </Form>
-        <p>Total Elements {response?.size || 0}</p>
-      </div>
+        <DataMessage>
+          {
+            statusResponse === 'loading' || statusResponse !== 'success' ?
+              'Buscando elementos...'
+              :
+              response && response.size === 0 ?
+                '¡No hay artículos relacionados con el término de búsqueda!'
+                :
+                'Elementos encontrados: ' + (response && response?.size || 0) 
+          }
+        </DataMessage>
+      </section>
       {
         statusResponse === 'loading' ?
 
-          <div className="flex flex-col justify-self-center items-center gap-2 pt-10">
+          <section className="flex flex-col justify-self-center items-center gap-2 pt-10">
             <Loader />
             <Button onClick={handleAbort} className="w-56 sm:w-96">Cancel</Button>
-          </div>
+          </section>
           :
-          statusResponse === 'success' && !response!.size ?
-
-            <NoData>¡No hay artículos relacionados con el término de búsqueda!</NoData>
-            :
-            <DataList data={response && response.data || []} />
+          <DataList data={response && response.data || []} />
       }
 
     </section>
