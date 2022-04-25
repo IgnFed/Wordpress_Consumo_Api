f2672f20684f0bf4da8ea8ce3529ee5463954e23
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-25 20:10:12 -0300

    interface now has more data

--- interfaces/DataResponse.interface.ts
+++ interfaces/DataResponse.interface.ts
@@ -1,12 +1,46 @@
+import { OnlyIncludeData } from "utils/types"
+import { Meta } from "./MetaTags.interface"
+
 type Categories = [
   {
     description: string,
     id: number,
     link: string,
     name: string,
-    slug: string
+    slug: string,
+    permalink: string
   }
 ]
+
+type AuthorProfession = {
+  standard: string,
+  signed: string
+}
+
+type AuthorSocialProfiles = {
+  facebook: string,
+  twitter: string,
+  instagram: string,
+  linkedin: string
+  youtube: string,
+  url: string,
+  pinterest: string
+}
+
+type Author = {
+  id: number,
+  link: string,
+  name: string,
+  permalink: string,
+  picture: string,
+  profession: AuthorProfession,
+  reviewer: boolean,
+  slug: string,
+  social_profiles: AuthorSocialProfiles,
+  summary: string,
+  type: string,
+  verified: boolean,
+}
 
 type FeatureMedia = {
   '2048x2048': string,
@@ -14,18 +48,39 @@ type FeatureMedia = {
   'thumbnail': string
 }
 
-export type Data = {
+type DataIncluded = "categories" | "excerpt" | "featured_media" | "headline" | "id" | "link" | 
+                    "permalink" | "slug" | "sponsor" | "title"
+type OtherAuthorPost = OnlyIncludeData<PostData, DataIncluded>
+type RelatedLinks = OnlyIncludeData<PostData, "id" | "link" | "permalink" | "slug" | "title"> & 
+{
+  description: string
+}
+
+
+export type PostData = {
+  author: Author,
   id: number,
   categories: Categories,
+  content: string,
   excerpt: string,
   featured_media: FeatureMedia,
   headline: string,
   link: string,
+  metas: Meta,
+  next_post: OtherAuthorPost,
+  next_posts: Array<OtherAuthorPost>,
+  permalink: string,
+  previous_post: OtherAuthorPost,
+  sponsor: string,
+  previous_posts: Array<OtherAuthorPost>,
+  published: string,
+  related_links: Array<RelatedLinks>,
   slug: string,
-  title: string
+  tags: Array<string>,
+  title: string,
 }
 
-export interface DataResponse<DataType = Array<Data>>{
+export interface DataResponse<DataType = Array<PostData>>{
   data: DataType,
   size: number,
   pages: number
