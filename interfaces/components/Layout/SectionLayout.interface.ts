27db5d1ad5cefd33184d405d824b01389ad22220
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-26 01:06:01 -0300

    layoutClass prop is not more a generic string

--- interfaces/components/Layout/SectionLayout.interface.ts
+++ interfaces/components/Layout/SectionLayout.interface.ts
@@ -1,8 +1,9 @@
 import { Meta } from "interfaces/MetaTags.interface";
+import { HTMLAttributes } from "react";
 
 type OptionalMeta = Partial<Meta>;
 
 export interface SectionLayout{
-  layoutClass?: string
+  layoutClass?: HTMLAttributes<HTMLElement>["className"];
   metas?: OptionalMeta
 }
