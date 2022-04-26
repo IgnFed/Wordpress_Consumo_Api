cec13a8da54a51d055857f0f58a85d646210ae17
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-26 02:25:45 -0300

    added props

--- components/post/Image.tsx
+++ components/post/Image.tsx
@@ -1,27 +1,37 @@
 import dynamic from 'next/dynamic';
+import { ImageProps as NextImageProps } from 'next/image';
 
 const NextImage = dynamic(()=> import('next/image')) 
 
 interface ImageProps {
   src: string,
   alt: string,
+  width?: string,
+  height?: string,
+  priority?: boolean,
+  layout?: NextImageProps["layout"]
 }
 
 export default function Image<Props extends ImageProps>(
   {
     src,
-    alt
+    alt,
+    width,
+    height,
+    layout,
+    priority
   }: Props
 ){
   return (
     <NextImage 
       src={src || "/public/images/default_image.png"}
       alt={alt}
-      loading='lazy'
+      loading={priority ? "eager" : "lazy"}
+      priority={priority || false}
       decoding='async'
-      layout='responsive'
-      width={"100%"}
-      height={"100%"}
+      layout={layout || 'responsive'}
+      width={width || "100%"}
+      height={height || "100%"}
     />
   )
 } 
