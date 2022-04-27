e4552e799d160d63c3cbfef2cbff412aee29cf6e
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-27 11:49:29 -0300

    Loader now accept className as prop

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,5 +1,12 @@
-export default function Loader() {
+interface LoaderProps {
+  className?: string
+}
+export default function Loader<Props extends LoaderProps>(
+  {
+    className=""
+  }: Props
+) {
   return (
-    <span className="w-10 h-10 animate-waving border-4 border-cyan-900 border-t-cyan-600 rounded-full" />
+    <span className={`w-10 h-10 animate-waving border-4 border-cyan-900 border-t-cyan-600 rounded-full ${className}`} />
   )
 }
