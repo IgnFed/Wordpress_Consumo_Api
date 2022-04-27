7b889e5a51ac9e585e4f3be51994b43626cca2b7
IgnFed <ignaciofedorenco2317@gmail.com>
2022-04-27 11:52:09 -0300

    Loader while page charge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import Loader from 'components/Loader'
 
 const Layout = dynamic(()=> import('components/Layout/main'))
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { events } = useRouter()
+  const [ loading, setLoading ] = useState(false)
+  useEffect(()=>{
+    events.on("routeChangeStart", ()=> setLoading(true))
+    events.on("routeChangeComplete", ()=> setLoading(false))
+    return ()=>{
+      events.off("routeChangeStart", ()=> setLoading(false))
+      events.off("routeChangeComplete", ()=> setLoading(false) )
+    }
+  }, [])
+  if(loading) return <Loader className="absolute flex left-2/4 top-2/4" />
   return (
     <Layout >
       <Component {...pageProps} />
